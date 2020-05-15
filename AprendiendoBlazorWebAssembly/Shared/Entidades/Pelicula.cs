ecbe9c54df616868cd6dd49e3520415104cbce42
diegoas1205 <diegoas1205@gmail.com>
2020-05-15 11:50:31 -0500

    Agregar archivos de proyecto.

---
+++ AprendiendoBlazorWebAssembly/Shared/Entidades/Pelicula.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace AprendiendoBlazorWebAssembly.Shared.Entidades
+{
+    public class Pelicula
+    {
+        public string Titulo { get; set; }
+        public DateTime Lanzamiento { get; set; }
+    }
+}
