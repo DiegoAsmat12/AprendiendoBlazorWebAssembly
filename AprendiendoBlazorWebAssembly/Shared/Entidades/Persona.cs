958012deca17d4389af276b9ba7b861d2b05ba41
diegoas1205 <diegoas1205@gmail.com>
2020-05-26 10:38:43 -0500

    Empezando a Implementar Persona.cs

---
+++ AprendiendoBlazorWebAssembly/Shared/Entidades/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprendiendoBlazorWebAssembly.Shared.Entidades
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Biografia { get; set; }
+        public string Foto { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
