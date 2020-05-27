9ae8cb1e003e972c1bace986821619275d349e12
diegoas1205 <diegoas1205@gmail.com>
2020-05-27 11:53:13 -0500

    Usando Markdown

--- AprendiendoBlazorWebAssembly/Shared/Entidades/Pelicula.cs
+++ AprendiendoBlazorWebAssembly/Shared/Entidades/Pelicula.cs
@@ -7,7 +7,11 @@ namespace AprendiendoBlazorWebAssembly.Shared.Entidades
 {
     public class Pelicula
     {
+        public int Id { get; set; }
         public string Titulo { get; set; }
+        public string Resume { get; set; }
+        public bool EnCartelera { get; set; }
+        public string Trailer { get; set; }
         public DateTime Lanzamiento { get; set; }
         public string Poster { get; set; }
         public string TituloCortado
