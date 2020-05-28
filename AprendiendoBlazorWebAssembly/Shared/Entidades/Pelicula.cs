60cd203d4d5519653605c98359f939268114d8d3
diegoas1205 <diegoas1205@gmail.com>
2020-05-28 11:58:53 -0500

    SeleccionMultiple

--- AprendiendoBlazorWebAssembly/Shared/Entidades/Pelicula.cs
+++ AprendiendoBlazorWebAssembly/Shared/Entidades/Pelicula.cs
@@ -17,6 +17,7 @@ namespace AprendiendoBlazorWebAssembly.Shared.Entidades
         [Required]
         public DateTime? Lanzamiento { get; set; }
         public string Poster { get; set; }
+        public List<GeneroPelicula> GenerosPelicula { get; set; } = new List<GeneroPelicula>();
         public string TituloCortado
         {
             get
