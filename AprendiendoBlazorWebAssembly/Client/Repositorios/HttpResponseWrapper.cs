1b70995e3bc3a66c90ca1aa2ed2387a0f6586a0c
diegoas1205 <diegoas1205@gmail.com>
2020-06-17 18:51:37 -0500

    Genero

---
+++ AprendiendoBlazorWebAssembly/Client/Repositorios/HttpResponseWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AprendiendoBlazorWebAssembly.Client.Repositorios
+{
+    public class HttpResponseWrapper<T>
+    {
+        public bool Error { get; set; }
+        public T Response { get; set; }
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+        public HttpResponseWrapper(T response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Error = error;
+            Response = response;
+            HttpResponseMessage = httpResponseMessage;
+        }
+    }
+}
--- AprendiendoBlazorWebAssembly/Client/Repositorios/IRepositorio.cs
+++ AprendiendoBlazorWebAssembly/Client/Repositorios/IRepositorio.cs
@@ -9,6 +9,6 @@ namespace AprendiendoBlazorWebAssembly.Client.Repositorios
     public interface IRepositorio
     {
         List<Pelicula> ObtenerPeliculas();
-
+        Task<HttpResponseWrapper<object>> Post<T>(string url, T enviar);
     }
 }
